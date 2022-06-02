1a3eb81b535d05da04f02554279a755fd3db5a06
BergPadda00 <jacqueslee0016@gmail.com>
2022-06-02 14:27:26 -0700

    0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
 /**
- * 	*	*main - Entry point1
- * 	 *	 *
- * 	  * 	  *Return Always 0 (Seccess)
- * 	  **/
+ ***main - Entry point1*
+ ***Return Always 0 (Seccess)
+ ***/
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 
 		return (0);
 }
