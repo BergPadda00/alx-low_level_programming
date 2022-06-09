beb23010f3f87e1979e08c654d4cc3d420185cf6
BergPadda00 <jacqueslee0016@gmail.com>
2022-06-09 00:46:30 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -13,7 +13,6 @@
  */
 
 void print_remaining_days(int month, int day, int year)
-
 {
 if (year % 4 == 0 || (year % 400 == 0 && year % 100 == 0))
 {
@@ -21,11 +20,10 @@ if (month > 2 && day >= 60)
 {
 day++;
 }
-
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
 }
-else 
+else
 {
 if (month == 2 && day == 60)
 {
