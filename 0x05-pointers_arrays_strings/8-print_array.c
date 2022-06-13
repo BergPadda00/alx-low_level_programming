3061e7d02f32e942e28db733eae14e605342ba13
BergPadda00 <jacqueslee0016@gmail.com>
2022-06-13 12:09:12 -0700

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,11 +9,11 @@
  */
 void print_array(int *a, int n)
 {
-int j;
-for (j = 0; j < n; j++)
+int i;
+for (i = 0; i < n; i++)
 {
-printf("%d", a[j]);
-if (j != (n - 1))
+printf("%d", a[i]);
+if (i < (n - 1))
 {
 printf(", ");
 }
