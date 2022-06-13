5af3719112e5e9a0d2dc92d57ecabcf2563c1830
BergPadda00 <jacqueslee0016@gmail.com>
2022-06-13 11:36:20 -0700

    0x05. C - Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,13 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strlen - Returns the length of a string
  * @s: String to count
  *
+ * int _strlen(char *s)
  * Return String (c)
+ *
  *
  */
 int _strlen(char *s)
