63bf7d5599f0cdc0fbcfb4d3818f88c3d8756157
BergPadda00 <jacqueslee0016@gmail.com>
2022-06-16 18:33:45 +0100

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,26 +7,23 @@
  * @c: char pointer to be  converted
  * Return: char pointer with converted string
  */
-char *leet(char *c)
+char *leet(char*)
 {
-int i = 0, j;
-char a[5][3] = {{'a', 'A', '4'},
-{'e', 'E', '3'},
-{'o', 'O', '0'},
-{'t', 'T', '7'},
-{'l', 'L', '1'}};
+	int i, x;
+	int find[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int replacer[] = {'4', '3', '0', '7', '1'};
 
-while (*(c + i) != '\0')
-{
-for (j = 0; j < 5; ++j)
-{
-if (*(c + i) == a[j][0] || *(c + i) == a[j][1])
-{
-*(c + i) = a[j][2];
-break;
-}
-}
-++i;
-}return (c)
+	for (i = 0; n[i] != '\0'; i++)
+	{
+	for (x = 0; x <= 9; x++)
+	{
+	if (n[i] == find[x])
+	{
+	n[i] = replacer[x / 2];
+	x = 9;
+	}
+	}
+	}
+	return (n);
 }
 
