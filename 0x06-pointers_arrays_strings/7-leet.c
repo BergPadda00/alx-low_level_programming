2eff761438aae5075fc4549bd83bd8d75ae636d2
BergPadda00 <jacqueslee0016@gmail.com>
2022-06-16 20:00:43 +0100

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,29 +1,27 @@
 #include "main.h"
-#include <stdio>
+#include <stdio.h>
 
 /**
- * leet - function that encodes a string into 1337
+ * leet - check the code for Holberton School students.
+ * @a: is a pointer type char
+ * Return: Always 0.
  *
- * @c: char pointer to be  converted
- * Return: char pointer with converted string
  */
-char *leet(char*)
+char *leet(char *a)
 {
-	int i, x;
-	int find[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-	int replacer[] = {'4', '3', '0', '7', '1'};
+char leet[] = "aAeEoOtTlL";
+char chan[] = "4433007711";
+int i, j;
 
-	for (i = 0; n[i] != '\0'; i++)
-	{
-	for (x = 0; x <= 9; x++)
-	{
-	if (n[i] == find[x])
-	{
-	n[i] = replacer[x / 2];
-	x = 9;
-	}
-	}
-	}
-	return (n);
+for (i = 0; a[i] != '\0'; i++)
+{
+
+for (j = 0; leet[j] != '\0'; j++)
+{
+if (a[i] == leet[j])
+a[i] = chan[j];
 }
 
+}
+return (a);
+}
