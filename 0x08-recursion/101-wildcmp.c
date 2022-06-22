6ca646c6a0fb441cab6da299ade2623affa851ae
BergPadda00 <jacqueslee0016@gmail.com>
2022-06-22 18:37:17 +0100

    0x08-recursion

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ * Write a function that compares two strings and returns 1
+ * if the strings can be considered identical, otherwise return 0.
+ *
+ * Return int wildcmp(char *s1, char *s2
+ *
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	int s1 = 1,*s2 = 2;
+
+	return (int wildcmp(char *s1, char *s2)
+			}
