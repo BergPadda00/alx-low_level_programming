7576f399ba5c2e9afc78bde34a64541b1da93819
BergPadda00 <jacqueslee0016@gmail.com>
2022-07-25 19:19:42 +0100

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,49 +1,37 @@
 #include "mainn.h"
 
 /**
- * read_textfile - function that reads a text file and prints it to the POSIX
- * standard output
- * @filename: Const char for name of file to be printed
- * @letters: Size_t for number of bytes to be printed
- * Return: Bytes read, 0 if error reading or writing to std output
+ * read_textfile - read text and print it to POSIX
+ * @filename: name of file char
+ * @letters:number of letters to read size_t
+ * Return: read the file display it or error 0
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t  n_read, n_write;
-	char *buf;
+	int fd, rd, wd;
+	char *buff = malloc(sizeof(char) * letters);
 
-	buf = malloc(sizeof(char) * (letters + 1));
-
-	if (buf == NULL)
-		return (0);
-
-	fd = open(filename, O_RDONLY);
-
-	if (fd == -1 || filename == NULL)
+	if (filename == NULL)
 	{
-		free(buf);
 		return (0);
 	}
 
-	n_read = read(fd, buf, letters);
-
-	if (n_read < 0)
+	fd = open(filename, O_RDWR);
+	if (fd == -1)
 	{
-		free(buf);
 		return (0);
 	}
-
-	n_write = write(STDOUT_FILENO, buf, n_read);
-
-	if (n_write < 0)
+	rd = read(fd, buff, letters);
+	if (rd == -1)
 	{
-		free(buf);
 		return (0);
 	}
-
+	wd = write(STDOUT_FILENO, buff, rd);
+	if (wd == -1)
+	{
+		return (0);
+	}
 	close(fd);
-	free(buf);
-	return (n_read);
+	free(buff);
+	return (wd);
 }
